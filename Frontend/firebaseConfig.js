e748894cecfd6c28fefb044dbe1b370f59436637
karthikrajasrc <karthikrajasrc@gmail.com>
2026-03-18 18:13:32 +0530

    key removed

--- Frontend/firebaseConfig.js
+++ Frontend/firebaseConfig.js
@@ -1,13 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBoo7Zl4MhKOVjiFe4HbjxEFL508fmLw1o",
+  apiKey: import.meta.env.VITE_GOOGLE_KEY,
   authDomain: "yugans-product.firebaseapp.com",
   projectId: "yugans-product",
   storageBucket: "yugans-product.firebasestorage.app",
